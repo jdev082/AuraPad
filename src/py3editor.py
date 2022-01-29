17ce6b92262a87f7817c4ec46ff9561fdee5b94c
JaydenDev <willsjayden2@gmail.com>
2022-01-29 14:43:02 -0600

    upd

--- src/py3editor.py
+++ src/py3editor.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import filedialog
 
 root = Tk()
-# use theme clam
 root.configure(background='#0D1117')
 root.title("py3editor")
 root.geometry("800x600")
@@ -13,7 +12,6 @@ def save_file(arg):
     file.write(text_editor.get(1.0, END))
     file.close()
 
-# create function to ask to open file
 def open_file(arg):
     file_name = filedialog.askopenfilename(defaultextension=".txt")
     file = open(file_name, "r")
@@ -21,17 +19,23 @@ def open_file(arg):
     text_editor.insert(1.0, file.read())
     file.close()
 
+def clear_text(arg):
+    text_editor.delete(1.0, END)
+
 save_button = Button(root, text="Save", command=lambda: save_file(text_editor))
 exit_button = Button(root, text="Exit", command=root.destroy)
 open_button = Button(root, text="Open", command=lambda: open_file(text_editor))
+clear_button = Button(root, text="Clear", command=lambda: clear_text(text_editor))
 
 save_button.configure(background='#0D1117', fg='#FFFFFF', highlightthickness='0', bd='0')
 open_button.configure(background='#0D1117', fg='#FFFFFF', highlightthickness='0', bd='0')
 exit_button.configure(background='#0D1117', fg='#FFFFFF', highlightthickness='0', bd='0')
+clear_button.configure(background='#0D1117', fg='#FFFFFF', highlightthickness='0', bd='0')
 
 open_button.grid(row=0, column=3)
 save_button.grid(row=0, column=2)
 exit_button.grid(row=0, column=1)
+clear_button.grid(row=0, column=4)
 
 text_editor = Text(root, width=100, height=30)
 text_editor.configure(background='#0D1117', highlightthickness='0', bd='0', fg='#FFFFFF')
